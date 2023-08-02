39fe696627390534ed3f4d8c71d8fbe2384e35fa
dyang21 <dyang32@binghamton.edu>
2023-08-01 20:18:35 -0400

    clean up code

--- python/data-generator.py
+++ python/data-generator.py
@@ -2,22 +2,30 @@ from kafka import KafkaProducer
 import time
 import json
 import random
-
+"""
+Returns dict
+"""
 def generate_sensor_data():
     data = {
         "temperature": random.uniform(20, 25),
         "humidity": random.uniform(30, 40),
         "timestamp": time.time()
     }
-    return data  # return as Python dict
+    return data 
 
-producer = KafkaProducer(
-    bootstrap_servers='my-kafka.default.svc.cluster.local:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+"""
 
+"""
+def main():
+    producer = KafkaProducer(
+        bootstrap_servers='my-kafka.default.svc.cluster.local:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
-while True:
-    data = generate_sensor_data()
-    producer.send('sensor-data', value=data)
-    print(f"Produced: {data}")
-    time.sleep(1)  # generate new data every second
+    while True:
+        data = generate_sensor_data()
+        producer.send('sensor-data', value=data)
+        print(f"Produced: {data}")
+        time.sleep(1) 
+
+if __name__ == "__main__":
+    main()
--- python/data-processor.py
+++ python/data-processor.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer
 import sqlite3
 import json
 
+<<<<<<< HEAD
 
 conn = sqlite3.connect('sensor_data.db')
 c = conn.cursor()
@@ -24,7 +25,37 @@ for message in consumer:
               (data["temperature"], data["humidity"], data["timestamp"]))
 
 
+=======
+db_path = os.path.join(os.sep, 'my-pv', 'sensor_data.db')
+
+def create_table():
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS sensor_data
+                (temperature real, humidity real, timestamp real)''')
+>>>>>>> 42f0e71 (clean up code)
     conn.commit()
+    return conn
 
+def main():
+    conn = create_table()
+    c = conn.cursor()
 
-conn.close()
+    consumer = KafkaConsumer(
+        'sensor-data',
+         bootstrap_servers='my-kafka.default.svc.cluster.local:9092',
+         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+    for message in consumer:
+        data = message.value
+        print(f"Consumed: {data}")
+
+        c.execute("INSERT INTO sensor_data VALUES (?, ?, ?)",
+                  (data["temperature"], data["humidity"], data["timestamp"]))
+
+        conn.commit()
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
